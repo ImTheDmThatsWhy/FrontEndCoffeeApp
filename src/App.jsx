e83047ea387f8925c7fefc880765c6e1164abd3e
jman322 <jack.vassallo01@gmail.com>
2025-03-18 22:18:15 +0800

    pages setup

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,6 @@
 import React from 'react'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import Home from './Components/Home/Home'
 import Contact from './Components/Contact/Contact'
 import Nav from './Components/Nav/Nav'
 import Footer from './Components/Footer/Footer'
@@ -6,11 +8,16 @@ import "@fontsource/inter";
 
 const App = () => {
   return (
-  <div className="parent-container">
-      <Nav />
-      <Contact />
-      <Footer />
-  </div>
+    <Router>
+      <div className="parent-container">
+        <Nav />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/contact" element={<Contact />} />
+        </Routes>
+        <Footer />
+      </div>
+    </Router>
   )
 }
 
