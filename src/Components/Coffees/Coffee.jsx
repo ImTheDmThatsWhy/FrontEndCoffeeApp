5d7f2add2d694abfdf10577bf6fe3f72c3acc8c2
jman322 <jack.vassallo01@gmail.com>
2025-03-20 20:22:37 +0800

    starting to mess with css a bit before moving to favs

--- src/Components/Coffees/Coffee.jsx
+++ src/Components/Coffees/Coffee.jsx
@@ -1,15 +1,33 @@
-import React from 'react'
 import './Coffee.css'
+import React, { useState, useEffect } from 'react'
 
 
-const Coffee = () => {
 
+function Coffee() {
+    const [records, setRecords] = useState([])
+
+    useEffect(()=>{
+        fetch('https://buzz-my-day-app-xaqh.onrender.com/coffee')
+        .then(response => response.json())
+        .then(data => setRecords(data))
+        .catch(err => console.log(err))
+    }, [])
 
     return (
-        <div>
-            fgwafwafwa
+        <div className='card-wrapper'>
+            {records.map((coffee, index) => (
+                <div className='card' key={index}>
+                    <h3>{coffee.name}</h3>
+                    <p>Brand: {coffee.brand}</p>
+                    <p>Type: {coffee.type}</p>
+                    <p>Description: {coffee.description}</p>
+                    <p>Cost: ${coffee.cost}</p>
+                    <p>Rating: {coffee.rating}/5</p>
+                </div>
+            ))}
         </div>
     )
 }
+
 
 export default Coffee
