ee857c5c68707cabdcf8cc196e465050f7d8b87e
Andrea Sutton <andrea.sutton@hotmail.com.au>
2025-03-21 17:03:43 +1100

    account page css styling fixed

--- src/account/account.jsx
+++ src/account/account.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import "./account.css";
+import placeholder from "../assets/placeholder.jpeg";
 // const AccountPage = () => {
 //         const [isCreateAccount, setIsCreateAccount]=
 //         useState(false)
@@ -11,34 +12,42 @@ const AccountPage = () => {
     return (
         <main>
             <div className="accountcontainer">
-                <h1 className="headeraccount">Account Details</h1>
-                <p>
-                    Welcome new coffee enthusiast! Please enter your details
-                    below.
-                </p>
-                <img
-                    className="profilepic"
-                    src="https://via.placeholder.com"
-                    alt="profile"
-                ></img>
-                <div>
-                    <input
-                        className="description"
-                        type="text"
-                        placeholder="description"
-                    ></input>
+                <div className="secondcontainer">
+                    <div className="headeraccount">Account Details</div>
+                    <p className="welcome">
+                        Welcome new coffee enthusiast! Please enter your details
+                        below.
+                    </p>
+                    <img
+                        className="profilepic"
+                        src={placeholder}
+                        alt="profile"
+                    ></img>
+                    <div>
+                        <textarea
+                            className="description"
+                            type="text"
+                            placeholder="description"
+                        ></textarea>
+                    </div>
                 </div>
                 <form>
                     <div className="form">
-                        <label for="displayname">Displayname:</label>
+                        <label id="label" for="displayname">
+                            displayname:
+                        </label>
                         <input id="displayname" className="input"></input>
                     </div>
                     <div className="form">
-                        <label for="name">name:</label>
+                        <label id="label" for="name">
+                            name:
+                        </label>
                         <input id="name" className="input"></input>
                     </div>
                     <div className="form">
-                        <label for="email">email:</label>
+                        <label id="label" for="email">
+                            email:
+                        </label>
                         <input id="email" className="input"></input>
                     </div>
                     <div className="Submit_container">
